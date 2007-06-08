a1b0bce03d5c997ef18800927be4e7294ff675ff
Mark Overmeer <mark@overmeer.net>
2007-06-08 15:10:06 +0000

    Import of MARKOV/Geo-Proj4-0.99 from CPAN.
    gitpan-cpan-distribution: Geo-Proj4
    gitpan-cpan-version:      0.99
    gitpan-cpan-path:         MARKOV/Geo-Proj4-0.99.tar.gz
    gitpan-cpan-author:       MARKOV
    gitpan-cpan-maturity:     released

--- lib/Geo/Proj4.pm
+++ lib/Geo/Proj4.pm
@@ -1,10 +1,10 @@
 # Copyrights 2007 by Mark Overmeer.
-# For other contributors see Changes.
+#  For other contributors see Changes.
 # See the manual pages for details on the licensing terms.
-# Pod stripped from pm file by OODoc 0.99.
+# Pod stripped from pm file by OODoc 1.00.
 package Geo::Proj4;
 use vars '$VERSION';
-$VERSION = '0.98';
+$VERSION = '0.99';
 
 use strict;
 use warnings;
